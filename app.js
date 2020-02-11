01e3b419820db5c86dc987322641c8a4f1c12bb0
bandiuka <bandiuka@gmail.com>
2020-02-11 10:41:16 +0800

    projectiin ehnii huvisagchdiig uusgesen

---
+++ app.js
@@ -0,0 +1,22 @@
+// Тоглогчийн ээлжийг хадгалах хувьсагч
+var activePlayer = 1;
+//Тоглогчийн цуглуулсан оноог хадгалах хувьсагч
+var scores = [0, 0];
+//Тоглогчийн ээлжиндээ цуглуулж байгаа оноог хадгалах хувьсагч
+var roundScore = 0;
+//Шооны аль талаараа буусныг хадгалах хувьсагч 1-6 оноо санамсаргүй бууна
+var dice = Math.floor(Math.random() * 6) + 1;
+
+// document.querySelector("#score-0").textContent = dice;
+
+// document.querySelector("#score-1").innerHTML = "<em> YES!<em>";
+
+document.querySelector("#score-0").textContent = 0;
+document.querySelector("#score-1").textContent = 0;
+
+document.querySelector("#current-0").textContent = 0;
+document.querySelector("#current-1").textContent = 0;
+
+document.querySelector(".dice").style.display = "none";
+
+console.log("Шоо:" + dice);
